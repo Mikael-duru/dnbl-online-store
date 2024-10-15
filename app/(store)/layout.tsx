b3a624de111ae933537f1b262c0fc3a1fa8d59af
Duru C. Michael <durumikaelc@gmail.com>
2024-10-15 11:53:45 +0100

    add border to dark mode

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -8,9 +8,11 @@ export default function MainLayout({
 }>) {
 	return (
 		<>
-			<Header />
-			{children}
-			<Footer />
+			<div className="dark:border-2 dark:border-dark-brown">
+				<Header />
+				{children}
+				<Footer />
+			</div>
 		</>
 	);
 }
