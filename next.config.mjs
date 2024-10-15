c89d2ab69def712fab16892880c4c5fd08cdf166
Duru C. Michael <durumikaelc@gmail.com>
2024-10-15 08:39:36 +0100

    update "images.domains to "images.remotePatterns"

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,29 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode:false,
+//   images: {
+//     domains: ['lh3.googleusercontent.com', 'graph.facebook.com'], // Allows Google user profile images
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode:false,
+  reactStrictMode: false,
   images: {
-    domains: ['lh3.googleusercontent.com', 'graph.facebook.com'], // Allows Google user profile images
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',  // Google user profile images
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'graph.facebook.com',  // Facebook profile images
+        pathname: '/**',
+      },
+    ],
   },
 };
 
